12f44a441a41459d23e4d4423141e836e8679e03
s.f.schouten <sfschouten@gmail.com>
2020-08-12 00:42:39 +0200

    entmax and experiments that use it

--- ane_research/models/modules/attention/activations.py
+++ ane_research/models/modules/attention/activations.py
@@ -3,7 +3,10 @@ from overrides import overrides
 
 from allennlp.common import Registrable
 from allennlp.nn.util import replace_masked_values
-from entmax import entmax15, sparsemax
+from allennlp.nn import util
+
+from entmax import entmax15, sparsemax, entmax_bisect
+
 import torch
 import torch.nn as nn
 
@@ -91,14 +94,14 @@ class Entmax15Activation(AttentionActivationFunction):
 @AttentionActivationFunction.register("entmax-alpha")
 class EntmaxAlphaActivation(AttentionActivationFunction):
 
-    def __init__(self, alpha, device):
-        super().__init__(self)
+    def __init__(self, alpha: float):
+        super().__init__()
 
-        self.alpha = torch.nn.Parameter(torch.tensor(alpha, requires_grad=True, device=device)) 
+        self.alpha = torch.nn.Parameter(torch.tensor(alpha, requires_grad=True)) 
 
     @overrides
     def forward(self, scores: torch.Tensor, mask: torch.Tensor, invert_mask: bool = True) -> torch.Tensor:
-        """Map a score vector to a probability distribution halfway between softmax and sparsemax
+        """Map a score vector to a probability distribution akin to softmax (alpha=1) and sparsemax (alpha=2)
 
         Args:
             scores (torch.Tensor): (Batch x Sequence Length)
@@ -109,10 +112,11 @@ class EntmaxAlphaActivation(AttentionActivationFunction):
                 PyTorch fills in things with a mask value of 1, AllenNLP modules expect the opposite
 
         Returns:
-            torch.Tensor: Distribution halfway between softmax and sparsemax
+            torch.Tensor: Distribution resulting from entmax with specified alpha
         """
         if invert_mask:
             masked_scores = replace_masked_values(scores, mask, -float("inf"))
         else:
             masked_scores = scores.masked_fill(mask, -float("inf"))
-        return entmax15(masked_scores, dim=-1)
+
+        return entmax_bisect(masked_scores, self.alpha, dim=-1)
